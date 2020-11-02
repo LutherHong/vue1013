ec47e3ec776ff55cb1884469a1169038bc114b1f
赵志宏(zhaozhihong) <zhaozhihong@inspur.com>
2020-11-02 20:29:53 +0800

    chore: (七) 图书页面设计

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,8 @@ import HelloWorld from '@/components/HelloWorld'
 import AppIndex from '@/components/home/AppIndex'
 import Login from '@/components/Login'
 import Home from '../components/Home'
+import LibraryIndex from "../components/library/LibraryIndex";
+
 Vue.use(Router)
 
 export default new Router({
@@ -32,6 +34,14 @@ export default new Router({
           path: '/index',
           name: 'AppIndex',
           component: AppIndex,
+          meta: {
+            requireAuth: true
+          }
+        },
+        {
+          path: '/library',
+          name: 'Library',
+          component: LibraryIndex,
           meta: {
             requireAuth: true
           }
